f4c3b7dee9126f5640230d7b78e9f9794e5483c3
animeshrick <animeshece198@gmail.com>
2023-02-05 14:22:48 +0530

    update ui

--- lib/screens/home/courses/courses.dart
+++ lib/screens/home/courses/courses.dart
@@ -1,87 +1,100 @@
 import 'package:flutter/material.dart';
 import 'package:silva/elements/buttons.dart';
 import '../../../elements/text_elements.dart';
+import 'details/course_details.dart';
 
 class Courses extends StatelessWidget {
   const Courses({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            customTextExtraBold('Courses', Colors.black, 16),
-            customTextMedium('View All', Colors.black45, 16),
-          ],
+    return InkWell(
+      onTap: ()=> Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => CourseDetails(),
         ),
-        const SizedBox(
-          height: 10,
-        ),
-        SizedBox(
-          height: 230,
-          child: ListView.separated(
-              shrinkWrap: true,
-              primary: false,
-              physics: const BouncingScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              separatorBuilder: (_, __) => const SizedBox(
-                    width: 10,
-                  ),
-              itemCount: 3,
-              itemBuilder: (context, int i) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Container(
-                      width: 230,
-                      height: 130,
-                      decoration: const BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(20),
-                        ),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                              "https://silvamethod.com/store/wp-content/uploads/2022/01/manifesting-course.jpg"),
-                          fit: BoxFit.cover,
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              customTextExtraBold('Courses', Colors.black, 16),
+              customTextMedium('View All', Colors.black45, 16),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            height: 230,
+            child: ListView.separated(
+                shrinkWrap: true,
+                primary: false,
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                separatorBuilder: (_, __) => const SizedBox(
+                      width: 10,
+                    ),
+                itemCount: 3,
+                itemBuilder: (context, int i) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Container(
+                        width: 250,
+                        height: 130,
+                        decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://silvamethod.com/store/wp-content/uploads/2022/01/manifesting-course.jpg"),
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    customTextBold(
-                        'Silva Life System', Colors.black, 18),
-                    const SizedBox(
-                      height: 5,
-                    ),
-
-                    Row(
-                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        customTextMedium('\$99', Colors.black, 14),
-                        const SizedBox(
-                          width: 85,
-                        ),
-                        customButton(
-                          height: 40,
-                          textColor: Colors.yellow,
-                          buttonText: 'Enroll Now',
-                          onPressed: () {},
-                          buttonTextSize: 18,
-                          btnColor: Colors.black,
-                        ),
-                      ],
-                    ),
-                  ],
-                );
-              }),
-        ),
-      ],
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          customTextBold('Silva Life System', Colors.black, 14),
+                          const SizedBox(
+                            width: 25,
+                          ),
+                          GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                width: 100,
+                                height: 30,
+                                decoration: const BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(5),
+                                  ),
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    "Enroll Now",
+                                    style: TextStyle(color: Colors.yellow),
+                                  ),
+                                ),
+                              )),
+                        ],
+                      ),
+                      customTextMedium('\$99', Colors.black, 14),
+                    ],
+                  );
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -14,51 +14,53 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0,
+    return SafeArea(
+      child: Scaffold(
           backgroundColor: Colors.white,
-          title: customTextBold('Home', Colors.black, 18),
-          centerTitle: true,
-          automaticallyImplyLeading: false,
-          iconTheme: IconThemeData(color: Colors.black),
-        ),
-        endDrawer: Drawer(),
-        body: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                DelayedDisplay(
-                  delay: Duration(milliseconds: 500),
-                  child: CustomImageSlider(
-                    images: [
-                      "https://silvamethod.com/store/wp-content/uploads/2022/01/manifesting-course.jpg",
-                      "https://silvamethod.com/store/wp-content/uploads/2022/01/manifesting-course.jpg"
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-
-                /// -------------- Courses ----------------
-                DelayedDisplay(
-                    delay: Duration(milliseconds: 1000), child: Courses()),
-
-                /// -------------- Blogs ----------------
-                DelayedDisplay(
-                    delay: Duration(milliseconds: 1500), child: Blogs()),
-
-                /// -------------- Events ----------------
-                DelayedDisplay(
-                    delay: Duration(milliseconds: 2000), child: Events()),
-              ],
-            ),
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            title: customTextBold('Home', Colors.black, 18),
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+            iconTheme: IconThemeData(color: Colors.black),
           ),
-        ));
+          endDrawer: Drawer(),
+          body: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  DelayedDisplay(
+                    delay: Duration(milliseconds: 500),
+                    child: CustomImageSlider(
+                      images: [
+                        "https://silvamethod.com/store/wp-content/uploads/2022/01/manifesting-course.jpg",
+                        "https://silvamethod.com/store/wp-content/uploads/2022/01/manifesting-course.jpg"
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  /// -------------- Courses ----------------
+                  DelayedDisplay(
+                      delay: Duration(milliseconds: 1000), child: Courses()),
+
+                  /// -------------- Blogs ----------------
+                  DelayedDisplay(
+                      delay: Duration(milliseconds: 1500), child: Blogs()),
+
+                  /// -------------- Events ----------------
+                  DelayedDisplay(
+                      delay: Duration(milliseconds: 2000), child: Events()),
+                ],
+              ),
+            ),
+          )),
+    );
   }
 }
